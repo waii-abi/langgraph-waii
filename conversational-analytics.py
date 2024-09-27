ca401c75bc396ae70a7c20839f66600df03116bc
Gunther Hagleitner <g@waii.ai>
2024-09-27 13:49:18 -0700

    cleanup

--- conversational-analytics.py
+++ conversational-analytics.py
@@ -3,6 +3,7 @@ import sys
 from typing import List, Optional, Dict, Any
 
 import pandas as pd
+import plotly
 from pydantic import BaseModel
 from langgraph.graph import StateGraph
 from langchain_openai import ChatOpenAI
@@ -16,7 +17,7 @@ class State(BaseModel):
     query: str = ''
     sql: str = ''
     data: List[Dict[str, Any]] = []
-    chart: str = ''
+    chart: Any = ''
     insight: str = ''
     response: str = ''
     error: Optional[str] = None
@@ -32,6 +33,7 @@ class LanggraphWorkflowManager:
         workflow = StateGraph(State)
 
         workflow.add_node("Question Classifier", self.question_classifier)
+        workflow.add_node("Result Classifier", self.result_classifier)
         workflow.add_node("SQL Generator", self.sql_generator)
         workflow.add_node("SQL Executor", self.sql_executor)
         workflow.add_node("Chart Generator", self.chart_gen)
@@ -45,12 +47,20 @@ class LanggraphWorkflowManager:
             {
                 "database": "SQL Generator",
                 "visualization": "Chart Generator",
-                "insight": "Insight Generator"
+                "general": "Insight Generator"
             }
         )
 
         workflow.add_edge("SQL Generator", "SQL Executor")
-        workflow.add_edge("SQL Executor", "Result Synthesizer")
+        workflow.add_edge("SQL Executor", "Result Classifier")
+        workflow.add_conditional_edges(
+            "Result Classifier",
+            lambda state: state.path_decision,
+            {
+                "visualization": "Chart Generator",
+                "data": "Result Synthesizer"
+            }
+        )
         workflow.add_edge("Chart Generator", "Result Synthesizer")
         workflow.add_edge("Insight Generator", "Result Synthesizer")
         workflow.add_edge("Result Synthesizer", "Question Classifier")
@@ -58,25 +68,22 @@ class LanggraphWorkflowManager:
         return workflow
 
     def question_classifier(self, state: State) -> State:
-
         state.database_description = self.format_catalog_info(WAII.Database.get_catalogs())
         state.query = input("Question: ")
 
         prompt = ChatPromptTemplate.from_messages([
             ("human",
              "Database info: \n---\n{database_description}\n---\n"
-             "Can you classify the following question into one of 'database' or 'insight'? Question: '{query}'. "
-             "Output: Strictly respond with either 'database', 'visualization', or 'insight'. No additional text.")
+             "Answer 'database' if this question is likely related to information in the database. Otherwise answer 'general'? Question: '{query}'. "
+             "Consider the information you have about the database, when in doubt answer 'database'")
         ])
         chain = prompt | ChatOpenAI() | StrOutputParser()
         classification = chain.invoke({"query": state.query, "database_description": state.database_description}).strip().lower()
-
         return state.model_copy(update={"path_decision": classification, "error": None})
 
     def sql_generator(self, state: State) -> State:
-        insight = ''
         sql = WAII.Query.generate(QueryGenerationRequest(ask=state.query)).query
-        return state.model_copy(update={"sql": sql})
+        return state.model_copy(update={"sql": sql, "insight":""})
 
     def sql_executor(self, state: State) -> State:
         data = WAII.Query.run(RunQueryRequest(query=state.sql)).rows
@@ -84,17 +91,27 @@ class LanggraphWorkflowManager:
 
     def chart_gen(self, state: State) -> State:
         df_data = pd.DataFrame(state.data)
-        chart = WAII.Chart.generate_chart(df=df_data).chart_spec
-        return state.model_copy(update={"chart": str(chart), "error": None}, deep=True)
+        chart = WAII.Chart.generate_chart(df=df_data)
+        return state.model_copy(update={"chart": chart.chart_spec, "error": None}, deep=True)
+
+    def result_classifier(self, state: State) -> State:
+        state.chart = ''
+        prompt = ChatPromptTemplate.from_messages([
+            ("human",
+             "Is the following question best answered by 'data' or a 'visualization'? Question: '{query}'. "
+             "Output: Strictly respond with either 'data', or 'visualization'. No additional text.")
+        ])
+        chain = prompt | ChatOpenAI() | StrOutputParser()
+        classification = chain.invoke({"query": state.query}).strip().lower()
+        return state.model_copy(update={"path_decision": classification, "error": None})
 
     def insight_generator(self, state: State) -> dict:
         prompt = ChatPromptTemplate.from_messages([("human", "{query}")])
         chain = prompt | ChatOpenAI() | StrOutputParser()
         insight = chain.invoke({"query": state.query})
-        return state.model_copy(update={"insight": insight, "error": None}, deep=True)
+        return state.model_copy(update={"insight": insight, "sql": "", "data": [], "error": None}, deep=True)
 
     def result_synthesizer(self, state: State) -> State:
-
         model = ChatOpenAI()
         prompt = ChatPromptTemplate.from_messages([
             ("system", "You are an expert assistant in analyzing data"),
@@ -106,6 +123,9 @@ class LanggraphWorkflowManager:
         chain = prompt | model | StrOutputParser()
         data = "\n".join(" | ".join(f"{key}: {value}" for key, value in row.items()) for row in state.data)
         output = chain.invoke({"query": state.query, "data": data, "insight": state.insight}).strip().lower()
+        if state.chart:
+            df = pd.DataFrame(state.data)
+            exec(state.chart.plot)
         print('Answer: '+output)
         return state.model_copy(update={"response": output}, deep=True)
 
@@ -130,7 +150,6 @@ class LanggraphWorkflowManager:
             try:
                 initial_state = State()
                 app_response = self.app.invoke(initial_state)
-                print(f"{app_response['response']}")
             except Exception as e:
                 print(f"Error in workflow: {e}. Will restart.")
 
